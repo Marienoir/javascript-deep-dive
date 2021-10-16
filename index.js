77c07bc528d8843ee7098663a94699a01c22109a
adaa <adaamgbede@gmail.com>
2021-10-16 01:27:57 +0100

    update changes

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(bodyParser.json());
 app.use("/api", router);
 
 const bootstrap = async () => {
-  try {
-    await connectDB; // connect to Database
-    app.listen(process.env.PORT || 3000);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB; // connect to Database
+        app.listen(process.env.PORT || 3000);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 bootstrap();
