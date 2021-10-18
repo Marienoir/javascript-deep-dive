ef4ca19fa622fdaa1227d1116b044045e6474c6a
adaa <adaamgbede@gmail.com>
2021-10-18 21:54:09 +0100

    reindent files

--- src/models/AppointmentModel.js
+++ src/models/AppointmentModel.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 
 const AppointmentSchema = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  userAvailabilityId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  reason: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    userAvailabilityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    reason: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
 });
 
 module.exports = mongoose.model("Appointment", AppointmentSchema);
--- src/models/UserAvailabilityModel.js
+++ src/models/UserAvailabilityModel.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const UserAvailabilitySchema = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
 
-  userAvailabilityId: {
-    type: mongoose.Schema.Types.ObjectId,
-    // required: true,
-  },
-  date: {
-    type: Date,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "booked"],
-    default: "pending",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+    userAvailabilityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        // required: true,
+    },
+    date: {
+        type: Date,
+    },
+    status: {
+        type: String,
+        enum: ["pending", "booked"],
+        default: "pending",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
 });
 
 module.exports = mongoose.model("UserAvailability", UserAvailabilitySchema);
