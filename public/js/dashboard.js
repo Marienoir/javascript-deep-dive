41ce411a99a26fc013842c8a4b60629893a1fbd9
adaa <adaamgbede@gmail.com>
2021-10-19 19:15:18 +0100

    work on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -13,10 +13,11 @@ fetch(`http://localhost:4000/api/user/dashboard/${username}`, {
 })
     .then(response => response.json())
     .then(data => {
-        let response = data.result;
-        if (response.length > 0) {
-            const newAppointment = response.map((item, i) => {
-                return `
+        if (data.status === true) {
+            let response = data.result;
+            if (response.length > 0) {
+                const newAppointment = response.map((item, i) => {
+                    return `
                     <div class="col-lg-3 col-md-6 mb-4">
                         <a href="/booking?id=${item?._id}" class="border rounded p-3 d-flex flex-column card-full">
                             <h3 class="fs-20 font-weight-bold text-black" >
@@ -26,10 +27,13 @@ fetch(`http://localhost:4000/api/user/dashboard/${username}`, {
                         </a>
                     </div>
                 `;
-            });
-            bodyContent.innerHTML = newAppointment.join(" ");
+                });
+                bodyContent.innerHTML = newAppointment.join(" ");
+            } else {
+                bodyContent.innerHTML = `<div>No Record Found</div>`
+            }
         } else {
-            bodyContent.innerHTML = `<div>No Record Found</div>`
+            alert('Error!!!' + data.message);
         }
     })
     .catch((error) => {
