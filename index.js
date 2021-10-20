adcba34bb1654b233e845e221f7b2e3c2a9c4526
adaa <adaamgbede@gmail.com>
2021-10-20 17:20:28 +0100

    update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/", viewRouter);
 const bootstrap = async () => {
     try {
         await connectDB; // connect to Database
-        app.listen(process.env.PORT || 3000);
+        app.listen(4000);
     } catch (error) {
         console.log(error);
     }
