d4974a1084a5383cd9978c68d78c21c7bf569e39
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2021-10-25 22:42:53 +0100

    removed white spaces

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const ServiceContainer = require("./services");
-
 const UserController = require("./controllers/UserController");
 const AppointmentController = require("./controllers/AppointmentController");
-
 const UserControllerHandler = UserController(ServiceContainer);
 const AppointmentControllerHandler = AppointmentController(ServiceContainer);
 
@@ -14,11 +12,9 @@ router.post("/user/availability/:username", (req, res) =>
 router.get("/user/dashboard/:username", (req, res) =>
     UserControllerHandler.dashboard(req, res)
 );
-
 router.post("/appointment/:userAvailabilityId", (req, res) =>
     AppointmentControllerHandler.bookAppointment(req, res)
 );
-
 router.get("/user/:username/appointments", (req, res) =>
     UserControllerHandler.getAllScheduledAppointments(req, res)
 );
