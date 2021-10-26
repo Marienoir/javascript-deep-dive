476a8c145a1774934d5f92d05763fbb1923971da
ChabuChabu <chabuchabu32@gmail.cpm>
2021-10-26 20:45:44 +0100

    booking feature modified

--- src/services/AppointmentService.js
+++ src/services/AppointmentService.js
@@ -4,64 +4,62 @@ const UserRepository = require("../repositories/UserRepository");
 const AppointmentRepository = require("../repositories/AppointmentRepository");
 
 const AppointmentService = () => {
-    const bookAppointment = async (userAvailabilityId, info) => {
-        const { name, email, reason } = info;
+  const bookAppointment = async (userAvailabilityId, info) => {
+    const { name, email, reason } = info;
 
-        const userAvailability = await UserAvailabilityModel.findOne({
-            _id: userAvailabilityId,
-            status: "pending",
-        });
+    const userAvailability = await UserRepository.getAllPendingAvailabilityById(
+      userAvailabilityId
+    );
 
-        if (!userAvailability) {
-            throw new Error("This user date isn't available for booking");
-        }
+    if (!userAvailability) {
+      throw new Error("This user date isn't available for booking");
+    }
+    const { userId } = userAvailability;
 
-        const { userId } = userAvailability;
+    let newAppointment = await AppointmentModel.create({
+      userId,
+      userAvailabilityId,
+      name,
+      email,
+      reason,
+    });
 
-        let newAppointment = await AppointmentModel.create({
-            userId,
-            userAvailabilityId,
-            name,
-            email,
-            reason,
-        });
+    userAvailability.status = "booked";
+    await userAvailability.save();
 
-        userAvailability.status = "booked";
-        await userAvailability.save();
+    if (!newAppointment) {
+      throw new Error("Appointment not scheduled successfully");
+    }
 
-        if (!newAppointment) {
-            throw new Error("Appointment not scheduled successfully");
-        }
+    return "Appointment booked successfully!!!";
+  };
 
-        return "Appointment booked successfully!!!";
-    };
+  const scheduledAppointments = async (params) => {
+    const { username } = params;
+    // check if the user exists in the database
+    const user = await UserRepository.findUserByUserName(username);
 
-    const scheduledAppointments = async (params) => {
-        const { username } = params;
-        // check if the user exists in the database
-        const user = await UserRepository.findUserByUserName(username);
+    // if user does not exist in the database throw new error
+    if (!user) {
+      throw new Error("User is not available");
+    }
 
-        // if user does not exist in the database throw new error
-        if (!user) {
-            throw new Error("User is not available");
-        }
+    // Getting all the scheduledAppointments of the user
+    const appointment =
+      await AppointmentRepository.getUserScheduledAppointments(user._id);
 
-        // Getting all the scheduledAppointments of the user
-        const appointment =
-            await AppointmentRepository.getUserScheduledAppointments(user._id);
+    // if there are no scheduled appointments for the user throw an error
+    if (!appointment) {
+      throw new Error("No scheduled appointments for this user");
+    }
 
-        // if there are no scheduled appointments for the user throw an error
-        if (!appointment) {
-            throw new Error("No scheduled appointments for this user");
-        }
+    return appointment;
+  };
 
-        return appointment;
-    };
-
-    return {
-        bookAppointment,
-        scheduledAppointments,
-    };
+  return {
+    bookAppointment,
+    scheduledAppointments,
+  };
 };
 
 module.exports = AppointmentService;
